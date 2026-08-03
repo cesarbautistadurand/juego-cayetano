50665c1718e4a35ce0eb5f7438d6525d68a9cdf7
cesarbautistadurand <155208859+cesarbautistadurand@users.noreply.github.com>
2026-08-03 01:22:39 -0500

    servidor.js

--- servidor.js
+++ servidor.js
@@ -9,7 +9,6 @@ let jugadores = {};
 let alienX = 0;
 let alienY = 0;
 
-// Control de estados del juego
 let estadoJuego = 'esperando'; // 'esperando', 'countdown', 'jugando', 'terminado'
 let segundosRestantes = 120;   // 2 minutos de partida
 let segundosCountdown = 10;    // 10 segundos de espera inicial
@@ -27,12 +26,10 @@ function enviarRanking() {
     io.emit('actualizarRanking', listaOrdenada);
 }
 
-// Inicia la cuenta regresiva de 10 segundos
 function iniciarCountdown() {
     estadoJuego = 'countdown';
     segundosCountdown = 10;
 
-    // Reseteamos los puntajes al iniciar una nueva ronda
     for (let id in jugadores) {
         jugadores[id].puntaje = 0;
     }
@@ -52,10 +49,9 @@ function iniciarCountdown() {
     }, 1000);
 }
 
-// Comienza los 2 minutos de juego
 function iniciarJuego() {
     estadoJuego = 'jugando';
-    segundosRestantes = 120; // 2 minutos
+    segundosRestantes = 120; 
     generarAlien();
     
     io.emit('comenzarPartida', { x: alienX, y: alienY });
@@ -72,7 +68,6 @@ function iniciarJuego() {
     }, 1000);
 }
 
-// Termina la ronda y define al ganador
 function finalizarJuego() {
     estadoJuego = 'terminado';
     let listaOrdenada = Object.values(jugadores).sort((a, b) => b.puntaje - a.puntaje);
@@ -80,7 +75,6 @@ function finalizarJuego() {
 
     io.emit('finPartida', { ganador: ganador.nombre, puntaje: ganador.puntaje });
 
-    // Esperamos 6 segundos mostrando al ganador y volvemos a iniciar la cuenta regresiva automáticamente
     setTimeout(() => {
         if (Object.keys(jugadores).length > 0) {
             iniciarCountdown();
@@ -96,7 +90,6 @@ io.on('connection', (socket) => {
         jugadores[socket.id] = { nombre: nombreAlumno, puntaje: 0 };
         enviarRanking();
 
-        // Si es el primer jugador en entrar, iniciamos la cuenta regresiva
         if (Object.keys(jugadores).length === 1 && estadoJuego === 'esperando') {
             iniciarCountdown();
         } else if (estadoJuego === 'countdown') {
@@ -109,7 +102,6 @@ io.on('connection', (socket) => {
     });
 
     socket.on('disparo', (intento) => {
-        // Solo se puede disparar si la partida está en curso
         if (!jugadores[socket.id] || estadoJuego !== 'jugando') return;
 
         if (intento.x === alienX && intento.y === alienY) {
@@ -131,7 +123,6 @@ io.on('connection', (socket) => {
         if(jugadores[socket.id]) {
             delete jugadores[socket.id];
             enviarRanking();
-            // Si ya no quedan jugadores, detenemos los relojes
             if (Object.keys(jugadores).length === 0) {
                 if (intervaloCountdown) clearInterval(intervaloCountdown);
                 if (intervaloJuego) clearInterval(intervaloJuego);
